78aa741373f2975364eb8139242d77995465b2b4
Saket Bhatnagar <47420921+SaketBhatnagar@users.noreply.github.com>
2024-03-03 21:52:19 +0530

    PWA added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+import PWA from 'next-pwa';
+
+const withPWA = PWA({
+    dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+    // register: true,
+    // scope: '/app',
+    // sw: 'service-worker.js',
+    //...
+    cacheOnFrontEndNav:true,
+
+  })
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA( nextConfig);
